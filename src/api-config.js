7d4c419e5b7adf1db190a949753e432389ecf9aa
Alejandro Jimenez <ajf@fastmail.com>
2021-02-11 12:40:12 -0600

    ADD book row component

--- src/api-config.js
+++ src/api-config.js
@@ -1,4 +1,6 @@
 const config = {
+  TOKEN: "CFPAT-LBtveUvtDi7YjAhsyNzZURthngcrVnIr53eOZjYnxuc",
+  BASE_URL: "https://api.contentful.com/",
   SPACE_ID: "1t4hjzo7y0kb",
   ENVIRONMENT: "master",
   CONTENT_TYPE_ID: "audiocontent-v8"
---
+++ src/components/AudiobookRow.jsx
@@ -0,0 +1,9 @@
+const AudiobookRow = (props) => {
+  console.log(props)
+  return(
+    <p>{props.title["es-MX"]}</p>
+  )
+
+}
+
+export default AudiobookRow;
