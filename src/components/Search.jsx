2b0bc424f15153ee05943882a843efead86313ac
Alejandro Jimenez <ajf@fastmail.com>
2021-02-11 12:40:34 -0600

    ADD a button to get the books

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,25 +1,41 @@
+import { useState } from 'react';
 import config from '../api-config';
+import AudiobookRow from './AudiobookRow';
 
 const Search = () => {
-  var requestOptions = {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer CFPAT-LBtveUvtDi7YjAhsyNzZURthngcrVnIr53eOZjYnxuc`
-    },
-    redirect: 'follow'
-  };
+  const [allAudiobooks, setAllAudiobooks] = useState([])
+
   
-  fetch(`https://api.contentful.com/spaces/${config.SPACE_ID}/environments/${config.ENVIRONMENT}/entries?select=fields,sys.id,sys.version&locale=es-MX&content_type=${config.CONTENT_TYPE_ID}`, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+  function getAllAudiobooks() {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${config.TOKEN}`
+      }
+    };
+
+    console.log('called')
+
+    fetch("https://api.contentful.com/spaces/1t4hjzo7y0kb/environments/master/entries?select=fields,sys.id,sys.version&locale=es-MX&content_type=audiocontent-v8", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        setAllAudiobooks(result.items)
+      })
+      .catch(error => console.log('error', error));
+  }
 
   return(
+    <>
     <form action="">
       <input type="text"/>
       <button type="submit">Search</button>
     </form>
+    <button onClick={getAllAudiobooks}>GetBooks</button>
+    {allAudiobooks.map(item =>
+      <AudiobookRow key={item.sys.id}  {...item.fields} />
+    )}
+    </>
   )
 }
 
