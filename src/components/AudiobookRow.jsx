a264ebedce02333d67de373d8db7bbf9dc851b2f
Alejandro Jimenez <ajf@fastmail.com>
2021-02-11 13:42:36 -0600

    REMOVE React-app styles

--- src/components/AudiobookRow.jsx
+++ src/components/AudiobookRow.jsx
@@ -1,10 +1,10 @@
 const AudiobookRow = (props) => {
   console.log(props.id)
   return(
-    <>
-    <p>{props.title["es-MX"]}</p>
-    <p>{props.authors["es-MX"][0]}</p>
-    </>
+    <div className="book-row">
+      <p>{props.title["es-MX"]}</p>
+      <p>{props.authors["es-MX"][0]}</p>
+    </div>
   )
 
 }
