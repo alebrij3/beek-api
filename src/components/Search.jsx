3060d0103ae0b47fa082af71da009ce2c6c38940
Alejandro Jimenez <ajf@fastmail.com>
2021-02-11 10:46:04 -0600

    CHANGE default port for development UPDATE App width

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,16 @@
 const Search = () => {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.contentful.com/spaces/xxxxxxxxxxxx/environments/xxxxxxx/entries?select=fields,sys.id,sys.version&locale=es-MX&content_type=xxxxxxxxxxx", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+  console.log(process.env.FOO)
+
   return(
     <form action="">
       <input type="text"/>
