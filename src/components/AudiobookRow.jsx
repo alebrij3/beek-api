664ba333c43bad302aa03bcb0efb17d9fbabfb00
Alejandro Jimenez <ajf@fastmail.com>
2021-02-11 17:02:08 -0600

    ADD removeAudiobook function

--- src/components/AudiobookRow.jsx
+++ src/components/AudiobookRow.jsx
@@ -1,5 +1,20 @@
 const AudiobookRow = (props) => {
   console.log(props.id)
+  const removeAudiobook = (id) => {
+    var raw = "";
+
+    var requestOptions = {
+      method: 'DELETE',
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch(`${config.BASE_URL}spaces/${config.SPACE_ID}/environments/${config.ENVIRONMENT}/entries/${id}`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+  }
   return(
     <div className="book-row">
       <p className="title">{props.title["es-MX"]}</p>
